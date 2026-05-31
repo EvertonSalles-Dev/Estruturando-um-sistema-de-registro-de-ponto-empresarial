b458b6107dc780a2d4fdeffccd7858407d78009b
Everton Dev Front-And <evertondamota12@gmail.com>
2026-05-31 14:26:48 -0300

    Delete PONTO directory

--- PONTO/Estruturando-um-sistema-de-registro-de-ponto-empresarial-main/python/README.md
+++
@@ -1,2 +0,0 @@
-# sistema-de-ponto-empresarial
-Estou estudando e desenvolvendo um sistema de registro de ponto para empresas 
--- PONTO/Estruturando-um-sistema-de-registro-de-ponto-empresarial-main/python/main.py
+++
@@ -1,9 +0,0 @@
-from banco import criar_tabelas
-from login import abrir_login
-import customtkinter as ctk
-
-ctk.set_appearance_mode("dark")
-ctk.set_default_color_theme("blue")
-
-criar_tabelas()
-abrir_login()
